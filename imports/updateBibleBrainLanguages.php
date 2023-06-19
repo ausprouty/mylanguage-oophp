cc1641db32fbbbc74851a8be8237062a664bf62c
ausprouty <bob.prouty@powertochange.org.au>
2023-06-19 15:08:18 +1000

    Updated Bible Brain Languages

--- imports/updateBibleBrainLanguages.php
+++ imports/updateBibleBrainLanguages.php
@@ -5,6 +5,8 @@ Get list of languages from https://www.faithcomesbyhearing.com/bible-brain-avail
 Download
 Open up Excell and then import as UTF-8 text
 Save in /imports/data as tab delimited
+
+In Feb 2023 this added 454 languages
 */
 $filename = ROOT . 'imports/data/BibleBrainLanguageUTF8.txt';
 $datafile = file_get_contents($filename);
@@ -13,7 +15,7 @@ $records = explode("\n", $datafile);
 foreach ($records as $record){
     $items = explode ("\t", $record);
     $languageCodeIso = $items[0];
-    $name = $items[1];
+    $name = str_replace('"', '', $items[1]);
     $dbConnection = new DatabaseConnection();
     $query = 'SELECT id FROM hl_languages 
         WHERE languageCodeIso = :languageCodeIso LIMIT 1';
@@ -21,6 +23,13 @@ foreach ($records as $record){
     $statement = $dbConnection->executeQuery($query, $params);
     $id = $statement->fetch(PDO::FETCH_COLUMN);
     if (!$id){
-        echo ("$languageCodeIso  $name<br> ");
+        $languageCodeHL = $languageCodeIso . '23';
+        $query = 'INSERT INTO hl_languages (languageCodeHL,  languageCodeIso, name) 
+        VALUES (:languageCodeHL, :languageCodeIso, :name)';
+        $params = array(':languageCodeHL' => $languageCodeHL,
+                        ':languageCodeIso' => $languageCodeIso,
+                        'name' => $name);
+        $statement = $dbConnection->executeQuery($query, $params);
+        
     }
 }
